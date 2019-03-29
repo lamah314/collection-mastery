0055f188d7f980110353dbb822c72c29ed3c8e5c
Anthony Lam <lamah314@gmail.com>
2019-03-29 11:10:19 -0400

    push files

---
+++ collection-mastery-api/src/main/java/org/wecancodeit/collectionmasteryapi/CollectionMasteryApiApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.collectionmasteryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionMasteryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionMasteryApiApplication.class, args);
+	}
+
+}
